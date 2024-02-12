0c25f68057eefcf7c0f79283c6eade7fd986d38a
Anthony Enedah <enedahanthony54@gmail.com>
2024-02-12 17:47:44 +0100

    Added contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Signin from './pages/SignIn';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
   </Router>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Contactpage from '../components/Contactpage';
+import Footer from '../components/Footer';
+export default function Contact() {
+  return (
+    <div>
+      <Header />
+      <Contactpage />
+      <Footer />
+    </div>
+  )
+}
