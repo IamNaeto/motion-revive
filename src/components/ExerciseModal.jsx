4753247c047f8d4ca794f648b8595a7f417902a9
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-02-18 12:14:12 -0800

    fixes

--- src/components/ExerciseModal.jsx
+++ src/components/ExerciseModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ExerciseModal = ({ name, gif, onClose, onFinished, countdown }) => {
+const ExerciseModal = ({ name, gif, onClose, onCompleted, countdown }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 z-50">
       <div className="grid items-center justify-center place-items-center gap-2 bg-white p-10 rounded-3xl max-w-[80%] max-h-[90%] relative">
@@ -14,7 +14,7 @@ const ExerciseModal = ({ name, gif, onClose, onFinished, countdown }) => {
             </div>
             <div className='flex items-center justify-center gap-4'>
               <button className="bg-[#EA7600] text-white py-2 px-5 rounded" onClick={onClose}>Stop</button>
-              <button className="bg-[#EA7600] text-white py-2 px-5 rounded" onClick={onFinished}>Finished</button>
+              <button className="bg-[#EA7600] text-white py-2 px-5 rounded" onClick={onCompleted}>Done</button>
             </div>
           </>
         )}
